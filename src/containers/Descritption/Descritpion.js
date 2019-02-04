049867e8a4a78ad9e626937a1535492875e51572
florovarelaa <florovarelaa@gmail.com>
2019-02-03 23:15:11 -0300

    Final Commit

--- src/containers/Descritption/Descritpion.js
+++ src/containers/Descritption/Descritpion.js
@@ -5,7 +5,7 @@ const Description = () => {
         <div className="bg-white tl">
             <p className="fw6">Learning React? Start Small.</p>
             <p>Can't pry yourself away from tutorials? The cure is to make tiny little experiment apps.</p>
-            <a src="www.google.com">Google</a>
+            <a className="black" href="www.google.com">Google</a>
         </div>
     )
 }
